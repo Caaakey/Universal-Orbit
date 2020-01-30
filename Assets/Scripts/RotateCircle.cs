dc1f6c20cc4782fe86fe2e6f93b360711c3a6003
kibongKim <kbop@naver.com>
2020-01-31 02:16:56 +0900

    Update Resources

---
+++ Assets/Scripts/RotateCircle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCircle : MonoBehaviour {
+
+    public float speed = .5f;
+    public Transform target = null;
+    public Vector3 point = Vector3.zero;
+    public Vector3 axis = Vector3.zero;
+
+	void Update ()
+    {
+        if (target != null)
+            transform.RotateAround(target.localPosition, axis, speed);
+        else if (point != Vector3.zero)
+            transform.RotateAround(point, axis, speed);
+        else
+            transform.Rotate(axis, speed);
+	}
+}
